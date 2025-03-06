9998123180f878c7319fcdd5d8c1d31850405437
codewithaseel21 <muhammadaseel21@gmail.com>
2025-03-06 18:16:19 +0500

    .....

---
+++ app.js
@@ -0,0 +1,263 @@
+
+
+// Question ------------------------ 01 Started
+
+// var num1 = 70;
+// if (num1 % 3 === 0 ) {
+//     console.log ("Number is divisible")
+
+// } else {
+//     console.log ("Number is not divisible")
+// }
+
+
+// ------------------------------------------------
+
+// Question - 02 Started
+// var num2 = 46;
+
+// if ( num2 % 2 === 0) {
+//     console.log ("Number is even")
+
+// } else {
+//     console.log ("Number is odd")
+// }
+// Question - 02 ended
+
+// --------------------------------------------------------------
+
+// Question - 03 Started
+// var age = 17.5;
+
+// if ( age > 18) {
+//     console.log ("Old enough");
+
+// }else {
+//     console.log ("Too Young")
+
+// }
+// Question - 03 ended
+
+// ----------------------------------------------------------------
+
+// Question - 04 Started
+// var Name = prompt("What is your name")
+
+// if (Name == "Aseel ahmed") {
+//     console.log ("Your name is like my name")
+
+// }
+// Question - 04 ended
+
+// ---------------------------------------------------------------
+
+// Question - 05 Started
+
+// var num1 = 70;
+
+// if (num1 % 3 === 0) {
+//   console.log("Number is divisible by 3");
+
+// } else {
+//   console.log("Number is not divisible by 3");
+
+// }
+// Question - 05 ended
+
+// ------------------------------------------------------------------
+
+// Question - 06 Started
+// var num1 = +prompt ("Enter Number 1")
+// var num2 = +prompt ("Enter Number 2")
+// var operator = prompt ("Enter (+ , - , * , / , %)")
+
+// if (operator === "+"){
+//     console.log ("Addition" , num1 + num2);
+
+// }else if (operator === "-"){
+//     console.log ("Subraction" , num1 - num2);
+
+// } else if (operator === "*"){
+//     console.log ("Multipy" , num1 * num2);
+
+// } else if (operator === "/"){
+//     console.log ("Divide" , num1 / num2);
+
+// } else if (operator === "%"){
+//     console.log ("Modulus" , num1 % num2);
+
+// } else {
+//     console.log ("Invalid Operator")
+// }
+// Question - 06 ended
+
+// ------------------------------------------------------------------
+
+// Question - 07 Started
+// var time = +prompt("Enter What time");
+
+// if (time >= 0000 && time < 1200) {
+//     console.log ("Good Morning");
+
+// }else if (time >= 1200 && time < 1700){
+//     console.log("Good Afternoon");
+
+// }else if (time >= 1700 && time < 2100){
+//     console.log("Good Evening");
+
+// }else if (time >= 2100 && time <= 2359) {
+//     console.log ("Good Night");
+
+// }else {
+//     console.log ("wrong past");
+// }
+// Question - 07 ended
+
+// -------------------------------------------------------------------
+
+// Question - 08 Started
+// var year = 2032;
+
+// if (year % 4 == 0){
+//     console.log("This is leap year")
+
+// }else {
+//     console.log("This is not a leap year");
+// }
+// Question - 08 ended
+
+// ------------------------------------------------------------------------------------------------
+// Question - 09 Started
+// var correct_password = ("aseelahmed21");
+
+// var user = prompt("Enter Password")
+
+// if (user === correct_password){
+//     console.log("Correct Password")
+
+// } else {
+//     console.log("Incorrect Password")
+// }
+// Question - 09 ended
+
+// -------------------------------------------------------------------
+// Question - 10 Started
+// var first_name = prompt("Enter Name");
+
+// if(first_name === "Fahad"){
+//     document.write("Hello Fahad!");
+
+// }else {
+//     document.write("You are not Fahad")
+// }
+// Question - 10 ended
+
+// ----------------------------------------------------------------
+
+// Question - 11 Started
+// var greeting;
+// var hour = 20;
+
+// if (hour < 18){
+//     console.log ("Good day");
+
+// }else {
+//     console.log ("Good evening ");
+
+// }
+// Question - 11 ended
+
+
+// ---------------------------------------------------------------------
+
+// Question - 11 Started
+// var num1 = 20;
+// var num2 = 20;
+
+// if (num1 < num2){
+//     console.log ("Num2 is greater than Num1: " + num2)
+
+
+// }else if (num1 > num2){
+//     console.log ("Num1 is greater than Num2: " + num1)
+
+// }else {
+//     console.log ("Both are Equal ")
+
+// }
+// Question - 11 ended
+
+
+// --------------------------------------------------------------
+// // Question - 12 Started
+// var number = 60 - 60;
+
+// if (number > 0){
+//    console.log ("Number is Positive ")
+
+// }else if (number < 0){
+//     console.log ("Number is Negative");
+
+// }else {
+//     console.log ("Numer is Zero")
+// }
+// // Question - 12 ended
+
+
+// ------------------------------------------------------------
+// // Question - 12 Started
+// var current_hour = prompt ("Enter Current Time");
+
+// if (current_hour >= "6am" && current_hour <= "9am") {
+//   console.log("Its Break Fast Time");
+
+// } else if (current_hour >= "11am" && current_hour <= "1pm") {
+//   console.log("Time for lanch");
+
+// } else if (current_hour >= "5pm" && current_hour <= "8pm") {
+//   console.log("It's Dinner Time");
+
+// }else {
+//     console.log ("Sorry, You'll have to wait")
+// }
+// // Question - 12 ended
+
+
+// ------------------------------------------------------------------------------------------------
+// // Question - 12 Started
+// var text_1 = 20;
+// var text_2 = "Aseel ahmed";
+// var text_3 = true;
+// var text_4;
+
+
+// console.log("Type of Content is: " + typeof text_1);
+// console.log("Type of Content is: " + typeof text_2);
+// console.log("Type of Content is: " + typeof text_3);
+// console.log("Type of Content is: " + typeof text_4);
+// // Question - 12 ended
+
+
+// ----------------------------------------------------------------
+// // Question - 13 Started
+// var vowel = prompt("Enter only 1 small character");
+
+// if (vowel == "a" || vowel == "e" || vowel == "i" || vowel == "o" || vowel == "u") {
+//     console.log("This is a vowel: " + true);
+
+// } else {
+//     console.log("This is not a vowel: " + false)
+// }
+// // // Question - 13 ended
+
+// ------------------------------------------------------------------
+// // Question - 14 Started
+//   var num1 = 20;
+// var num2 = 10;
+
+// if (num1 !== num2 ){
+//     console.log("The answer is: " + true );
+
+// }else {
+//     console.log("The answer is: " + false);
+// }
